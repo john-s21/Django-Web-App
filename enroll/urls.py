affc6eb509713fb8ac501c3b532fd6da03f673c9
john-s21 <bijudonbosco1995@gmail.com>
2021-01-18 22:49:29 +0530

    main theme changed

--- enroll/urls.py
+++ enroll/urls.py
@@ -4,10 +4,10 @@ from .import views
 
 urlpatterns = [
     path('', views.H.as_view(), name='home'),
-    path('registration/', views.N.as_view(), name='new'),
-    path('records/', views.R.as_view(), name='records'),
-    path('update/<int:pk>/', views.U.as_view(), name='edit'),
-    path('delete/<int:pk>/', views.D.as_view(), name='delete'),
+    path('registration', views.N.as_view(), name='new'),
+    path('records', views.R.as_view(), name='records'),
+    path('update/<int:pk>', views.U.as_view(), name='edit'),
+    path('delete/<int:pk>', views.D.as_view(), name='delete'),
     path('contact.html', views.CN.as_view(), name='contact'),
     path('log.html', views.log, name='sign'),
 ]
