d393de04a5a2e94d3b058d6eff068738ecea8344
john-s21 <bijudonbosco1995@gmail.com>
2021-02-23 21:04:14 +0530

    all ok

--- student/urls.py
+++ student/urls.py
@@ -21,7 +21,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('enroll.urls')),
+    path('', include('enroll.urls')),
 
 ]
 if settings.DEBUG:
