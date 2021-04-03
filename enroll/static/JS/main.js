8c37dd4caf6554ebdbe3acdcfd5364a79a3d4a9c
john-s21 <bijudonbosco1995@gmail.com>
2021-04-03 22:38:50 +0530

    correction required for the margins and added goto top button

--- enroll/static/JS/main.js
+++ enroll/static/JS/main.js
@@ -1,26 +1,41 @@
-function con()
-    {
-        alert("button testing");
-    }
+var mybutton = document.getElementById("tpbtn");
+window.onscroll = function() {scrollFunction()};
 
-function conf()
-{
-  if (confirm("AUTHENTICATION REQUIRED"))
-  {
-    var u = prompt("User Name");
-    var p = prompt("Password");
-    if (u != null)
-    {
-      document.getElementById("usr").innerHTML = u;
-      document.getElementById("pwd").innerHTML = p;
-    }
-    else
-    {
-      txt = "You pressed Cancel!";
-    }
-  }
-  else
-  {
-    txt = "You pressed Cancel!";
-  }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+  {mybutton.style.display = "block";}
+   else {mybutton.style.display = "none";}
 }
+
+function tp() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//function con()
+//    {
+//        alert("button testing");
+//    }
+//
+//function conf()
+//{
+//  if (confirm("AUTHENTICATION REQUIRED"))
+//  {
+//    var u = prompt("User Name");
+//    var p = prompt("Password");
+//    if (u != null)
+//    {
+//      document.getElementById("usr").innerHTML = u;
+//      document.getElementById("pwd").innerHTML = p;
+//    }
+//    else
+//    {
+//      txt = "You pressed Cancel!";
+//    }
+//  }
+//  else
+//  {
+//    txt = "You pressed Cancel!";
+//  }
+//}
+
